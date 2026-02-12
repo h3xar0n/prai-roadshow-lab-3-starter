afa6cb201a6c63a94143190474c778f8891211e0
noɿA <aronone@google.com>
2026-02-12 06:35:45 -0600

    Remove Cloud Run Source Deploy repository resource
    Removed the google_artifact_registry_repository resource block for Cloud Run Source Deploy.

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,15 +10,3 @@ provider "google" {
 
 // Task 2: Add the Model Armor template
 // add Model Armor template here
-
-// leave this part:
-resource "google_artifact_registry_repository" "cloud_run_source_deploy" {
-  location      = var.region
-  repository_id = "cloud-run-source-deploy"
-  description   = "Repository for Cloud Run Source Deploy"
-  format        = "DOCKER"
-
-  labels = {
-    "dev-tutorial" = "prod-ready-3"
-  }
-}
